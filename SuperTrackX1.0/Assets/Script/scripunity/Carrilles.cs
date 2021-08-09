24635bcb6e6aadea44156dde5e61748c0e93ee43
otaro36 <68829944+otaro36@users.noreply.github.com>
2021-08-09 18:22:19 -0500

    a

---
+++ SuperTrackX1.0/Assets/Script/scripunity/Carrilles.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrilles : MonoBehaviour
+{
+    [SerializeField] private float m_PersistTime;
+
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (transform.parent.parent == null)
+            {
+                Destroy(gameObject, m_PersistTime);
+            }
+        }
+    }
+}
+
